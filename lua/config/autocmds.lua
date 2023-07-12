b0b57139a3f182c66152d7dbe0fb556bb82ad45f
Elio <ElioStalteri@users.noreply.github.com>
2023-07-12 10:52:09 +0100

    initial

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,17 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+--
+
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
+  group = augroup("*"),
+  --command = "",
+  callback = function()
+    vim.cmd("normal zR")
+  end,
+})
