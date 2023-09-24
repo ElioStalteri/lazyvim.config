db660d48b91f8d124f1eb2249ee1bbd9a4c34ef7
Your Name <you@example.com>
2023-09-24 19:59:53 +0100

    changed theme

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -15,10 +15,11 @@ return {
     },
   },
   { "ellisonleao/gruvbox.nvim" },
+  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "nightfly",
     },
   },
   {
