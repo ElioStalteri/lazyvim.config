cc8a9af3ad5fdbeadc714168221f47b6a9727d20
Elio <ElioStalteri@users.noreply.github.com>
2023-11-07 19:11:25 +0000

    removed pretty icons from neorg

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -168,7 +168,7 @@ return {
       require("neorg").setup({
         load = {
           ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          -- ["core.concealer"] = {}, -- Adds pretty icons to your documents
           -- ["core.presenter"] = {
           --   config = {
           --     zen_mode = "zen-mode",
