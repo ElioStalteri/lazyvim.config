be8e9054263ee38329950ee2b87b8fae1605d49e
Your Name <you@example.com>
2024-01-25 00:35:10 +0000

    changed keymap for getting the references

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -8,6 +8,14 @@ return {
   },
   { "mbbill/undotree" },
   {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- change a keymap
+      keys[#keys + 1] = { mode = { "n" }, "gr", "<cmd>Trouble lsp_references<cr>", desc = "References" }
+    end,
+  },
+  {
     "Iron-E/nvim-highlite",
     config = function()
       -- OPTIONAL: setup the plugin. See "Configuration" for information
