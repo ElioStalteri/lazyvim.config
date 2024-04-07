8ab9ca8a8b752e9adad78c2ebd9b96f4cd762929
Your Name <you@example.com>
2024-04-07 23:01:13 +0100

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,5 @@ vim.opt.foldmethod = "indent"
 vim.opt.foldenable = false
 vim.opt.foldlevel = 99
 vim.g.markdown_folding = 1 -- enable markdown folding
+vim.g.autoformat = false
+vim.g.lazygit_config = false
