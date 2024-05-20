b9cd4bb845a99c17c9d8afce2714172d93a8f000
Elio <ElioStalteri@users.noreply.github.com>
2024-05-20 08:59:58 +0100

    remove unused function

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -1,45 +1,3 @@
-function hslToRgb(h, s, l)
-  h = h / 360
-  s = s / 100
-  l = l / 100
-
-  local r, g, b
-
-  if s == 0 then
-    r, g, b = l, l, l -- achromatic
-  else
-    local function hue2rgb(p, q, t)
-      if t < 0 then
-        t = t + 1
-      end
-      if t > 1 then
-        t = t - 1
-      end
-      if t < 1 / 6 then
-        return p + (q - p) * 6 * t
-      end
-      if t < 1 / 2 then
-        return q
-      end
-      if t < 2 / 3 then
-        return p + (q - p) * (2 / 3 - t) * 6
-      end
-      return p
-    end
-
-    local q = l < 0.5 and l * (1 + s) or l + s - l * s
-    local p = 2 * l - q
-    r = hue2rgb(p, q, h + 1 / 3)
-    g = hue2rgb(p, q, h)
-    b = hue2rgb(p, q, h - 1 / 3)
-  end
-
-  if not a then
-    a = 1
-  end
-  return r * 255, g * 255, b * 255, a * 255
-end
-
 local logo = [[
                                                                                   
                      ████ ██████           █████      ██                    
@@ -60,8 +18,6 @@ local logo = [[
         ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝         ╚═╝   ╚═╝       ╚══════╝╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝
                                                                                                   
 ]]
-
-local function get_color_number(i, rand) end
 
 -- Map over the headers, setting a different color for each line.
 -- This is done by setting the Highligh to gradientN, where N is the row index.
