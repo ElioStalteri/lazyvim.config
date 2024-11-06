bcb930d6f31623a2f107d0596b32c642ae310d64
elio <elio@localhost.localdomain>
2024-11-06 15:43:38 +0000

    added yanky and find and replace

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -92,35 +92,44 @@ return {
       need = 1,
       branch = true, -- use git branch to save session
     },
-    -- keys = {
-    --   {
-    --     "<leader>qs",
-    --     function()
-    --       require("persistence").load()
-    --     end,
-    --     desc = "Restore Session",
-    --   },
-    --   {
-    --     "<leader>qS",
-    --     function()
-    --       require("persistence").select()
-    --     end,
-    --     desc = "Select Session",
-    --   },
-    --   {
-    --     "<leader>ql",
-    --     function()
-    --       require("persistence").load({ last = true })
-    --     end,
-    --     desc = "Restore Last Session",
-    --   },
-    --   {
-    --     "<leader>qd",
-    --     function()
-    --       require("persistence").stop()
-    --     end,
-    --     desc = "Don't Save Current Session",
-    --   },
-    -- },
+  },
+  {
+    "MagicDuck/grug-far.nvim",
+    config = function()
+      require("grug-far").setup({
+        -- options, see Configuration section below
+        -- there are no required options atm
+        -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+      })
+    end,
+    keys = {
+      { "<leader>sr", "<cmd>GrugFar<cr>", desc = "Open find and replace" },
+    },
+  },
+  {
+    "gbprod/yanky.nvim",
+    recommended = true,
+    desc = "Better Yank/Paste",
+    -- event = "LazyFile",
+    opts = {
+      highlight = { timer = 150 },
+    },
+    keys = {
+      {
+        "<leader>p",
+        function()
+          require("telescope").extensions.yank_history.yank_history({})
+        end,
+        mode = { "n", "x" },
+        desc = "Open Yank History",
+      },
+      { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank Text" },
+      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put Text After Cursor" },
+      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Cursor" },
+      { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put Text After Selection" },
+      { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Selection" },
+      { "[y", "<Plug>(YankyCycleForward)", desc = "Cycle Forward Through Yank History" },
+      { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle Backward Through Yank History" },
+    },
   },
 }
