04e18a3f68043de745d422f265525854ad5827e7
elio <elio@localhost.localdomain>
2024-11-12 01:59:12 +0000

    added neogit

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -132,4 +132,17 @@ return {
       { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle Backward Through Yank History" },
     },
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+      "echasnovski/mini.pick", -- optional
+    },
+    config = true,
+  },
 }
