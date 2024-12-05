6e6741afa447a1b690d7cbef8ccfead8235de14e
elio <elio@localhost.localdomain>
2024-12-05 23:09:36 +0000

    fixed deno lsp config

--- init.lua
+++ init.lua
@@ -569,6 +569,7 @@ require("lazy").setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      local lspConfig = require("lspconfig")
       local servers = {
         -- clangd = {},
         gopls = {},
@@ -582,7 +583,9 @@ require("lazy").setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        denols = {
+          root_dir = lspConfig.util.root_pattern("deno.json", "deno.jsonc"),
+        },
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
