40b5b9723a757e10bd81f659d7ca5c653c7cd39b
elio <elio@localhost.localdomain>
2024-12-09 02:10:48 +0000

    removed other stuff apart from lazygit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -19,97 +19,23 @@ return {
       { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     },
   },
-  {
-    "sindrets/diffview.nvim",
-    opts = {
-      view = {
-        merge_tool = {
-          layout = "diff1_plain",
-          disable_diagnostics = true, -- Temporarily disable diagnostics for diff buffers while in the view.
-          winbar_info = true, -- See |diffview-config-view.x.winbar_info|
-        },
-      },
-      keymaps = {
-        disable_defaults = true, -- Disable the default keymaps
-      },
-    },
-    keys = {
-      { "<leader>go", "<cmd>DiffviewOpen<cr>", desc = "Diffview open" },
-      { "<leader>ge", "<cmd>DiffviewClose<cr>", desc = "Diffview close" },
-      { "<leader>gf", "<cmd>DiffviewToggleFiles<cr>", desc = "Diffview files toggle" },
-      -- require("diffview.actions")
-      {
-        "<leader>gco",
-        function()
-          require("diffview.actions").conflict_choose("ours")
-        end,
-        desc = "Choose the OURS version of a conflict",
-      },
-      {
-        "<leader>gct",
-        function()
-          require("diffview.actions").conflict_choose("theirs")
-        end,
-        desc = "Choose the THEIRS version of a conflict",
-      },
-      {
-        "<leader>gcb",
-        function()
-          require("diffview.actions").conflict_choose("base")
-        end,
-        desc = "Choose the BASE version of a conflict",
-      },
-      {
-        "<leader>gca",
-        function()
-          require("diffview.actions").conflict_choose("all")
-        end,
-        desc = "Choose all the versions of a conflict",
-      },
-      {
-        "<leader>gcx",
-        function()
-          require("diffview.actions").conflict_choose("none")
-        end,
-        desc = "Delete the conflict region",
-      },
-      {
-        "<leader>gcO",
-        function()
-          require("diffview.actions").conflict_choose_all("ours")
-        end,
-        desc = "Choose the OURS version of a conflict for the whole file",
-      },
-      {
-        "<leader>gcT",
-        function()
-          require("diffview.actions").conflict_choose_all("theirs")
-        end,
-        desc = "Choose the THEIRS version of a conflict for the whole file",
-      },
-      {
-        "<leader>gcB",
-        function()
-          require("diffview.actions").conflict_choose_all("base")
-        end,
-        desc = "Choose the BASE version of a conflict for the whole file",
-      },
-      {
-        "<leader>gcA",
-        function()
-          require("diffview.actions").conflict_choose_all("all")
-        end,
-        desc = "Choose all the versions of a conflict for the whole file",
-      },
-      {
-        "<leader>gcX",
-        function()
-          require("diffview.actions").conflict_choose_all("none")
-        end,
-        desc = "Delete the conflict region for the whole file",
-      },
-    },
-  },
+  -- {
+  --   "sindrets/diffview.nvim",
+  --   opts = {
+  --     view = {
+  --       merge_tool = {
+  --         layout = "diff1_plain",
+  --         disable_diagnostics = true, -- Temporarily disable diagnostics for diff buffers while in the view.
+  --         winbar_info = true, -- See |diffview-config-view.x.winbar_info|
+  --       },
+  --     },
+  --   },
+  --   keys = {
+  --     { "<leader>go", "<cmd>DiffviewOpen<cr>", desc = "Diffview open" },
+  --     { "<leader>ge", "<cmd>DiffviewClose<cr>", desc = "Diffview close" },
+  --     { "<leader>gf", "<cmd>DiffviewToggleFiles<cr>", desc = "Diffview files toggle" },
+  --   },
+  -- },
   -- {
   --   "tpope/vim-fugitive",
   --   opts = {},
