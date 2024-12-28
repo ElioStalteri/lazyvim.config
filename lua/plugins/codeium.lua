98945061182f38f161dd30c2be73a60800b79b37
elio <elio@localhost.localdomain>
2024-12-28 17:41:32 +0000

    refactor

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -5,54 +5,19 @@ return {
       "nvim-lua/plenary.nvim",
       "hrsh7th/nvim-cmp",
     },
-    config = function()
-      require("codeium").setup({
-        -- Optionally disable cmp source if using virtual text only
-        enable_cmp_source = false,
-        virtual_text = {
-          enabled = true,
-
-          -- These are the defaults
-
-          -- Set to true if you never want completions to be shown automatically.
-          manual = false,
-          -- A mapping of filetype to true or false, to enable virtual text.
-          filetypes = {},
-          -- Whether to enable virtual text of not for filetypes not specifically listed above.
-          default_filetype_enabled = true,
-          -- How long to wait (in ms) before requesting completions after typing stops.
-          idle_delay = 75,
-          -- Priority of the virtual text. This usually ensures that the completions appear on top of
-          -- other plugins that also add virtual text, such as LSP inlay hints, but can be modified if
-          -- desired.
-          virtual_text_priority = 65535,
-          -- Set to false to disable all key bindings for managing completions.
-          map_keys = true,
-          -- The key to press when hitting the accept keybinding but no completion is showing.
-          -- Defaults to \t normally or <c-n> when a popup is showing.
-          accept_fallback = nil,
-          -- Key bindings for managing completions in virtual text mode.
-          key_bindings = {
-            -- Accept the current completion.
-            accept = "<C-y>",
-            -- Accept the next word.
-            accept_word = false,
-            -- Accept the next line.
-            accept_line = false,
-            -- Clear the virtual text.
-            clear = "<C-c>",
-            -- Cycle to the next completion.
-            next = "<M-]>",
-            -- Cycle to the previous completion.
-            prev = "<M-[>",
-          },
-        },
-      })
-    end,
+    event = "BufEnter",
+    -- config = function()
+    --   vim.g.codeium_no_map_tab = true
+    -- end,
     keys = {
       { "<leader>ac", "<cmd>Codeium Chat<cr>", desc = "open chat" },
       { "<leader>aa", "<cmd>Codeium Auth<cr>", desc = "auth" },
       { "<leader>at", "<cmd>Codeium Toggle<cr>", desc = "Toggle" },
+      { mode = { "i" }, "<C-c>", "<Cmd>call codeium#Clear()<CR>", desc = "Clear suggestion" },
+      -- WARN: <C-y> does'n work :(
+      -- { mode = { "i" }, "<C-y>", "<Cmd>call codeium#Accept()<CR>", desc = "Accept suggestion" },
+      { mode = { "i" }, "<C-n>", "<Cmd>call codeium#CycleCompletions(1)<CR>", desc = "Next suggestion" },
+      { mode = { "i" }, "<C-p>", "<Cmd>call codeium#CycleCompletions(-1)<CR>", desc = "Previous suggestion" },
     },
   },
 }
