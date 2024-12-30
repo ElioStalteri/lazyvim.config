b346df65f20a337765edc4e99dafdc064ffa40d2
elio <elio@localhost.localdomain>
2024-12-30 11:07:26 +0000

    added gemini ai to neovim

--- init.lua
+++ init.lua
@@ -879,7 +879,7 @@ require("lazy").setup({
   require("plugins.neotree"),
   require("plugins.git"),
   require("plugins.snacks"),
-  require("plugins.codeium"),
+  require("plugins.AI"),
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
