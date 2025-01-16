eec0cd27b33782d2daa862296b20a76eef2335f5
elio <elio@localhost.localdomain>
2025-01-16 17:25:20 +0000

    removed codeium because it's annoing

--- lua/plugins/AI.lua
+++ lua/plugins/AI.lua
@@ -1,25 +1,25 @@
 return {
-  {
-    "Exafunction/codeium.vim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    event = "BufEnter",
-    -- config = function()
-    --   vim.g.codeium_no_map_tab = true
-    -- end,
-    keys = {
-      -- { "<leader>ac", "<cmd>Codeium Chat<cr>", desc = "open chat" },
-      { "<leader>aa", "<cmd>Codeium Auth<cr>", desc = "Codeium Auth" },
-      { "<leader>at", "<cmd>Codeium Toggle<cr>", desc = "Codeium Toggle" },
-      { mode = { "i" }, "<C-c>", "<Cmd>call codeium#Clear()<CR>", desc = "Clear suggestion" },
-      -- WARN: <C-y> does'n work :(
-      -- { mode = { "i" }, "<C-y>", "<Cmd>call codeium#Accept()<CR>", desc = "Accept suggestion" },
-      { mode = { "i" }, "<C-n>", "<Cmd>call codeium#CycleCompletions(1)<CR>", desc = "Next suggestion" },
-      { mode = { "i" }, "<C-p>", "<Cmd>call codeium#CycleCompletions(-1)<CR>", desc = "Previous suggestion" },
-    },
-  },
+  -- {
+  --   "Exafunction/codeium.vim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "hrsh7th/nvim-cmp",
+  --   },
+  --   event = "BufEnter",
+  --   -- config = function()
+  --   --   vim.g.codeium_no_map_tab = true
+  --   -- end,
+  --   keys = {
+  --     -- { "<leader>ac", "<cmd>Codeium Chat<cr>", desc = "open chat" },
+  --     { "<leader>aa", "<cmd>Codeium Auth<cr>", desc = "Codeium Auth" },
+  --     { "<leader>at", "<cmd>Codeium Toggle<cr>", desc = "Codeium Toggle" },
+  --     { mode = { "i" }, "<C-c>", "<Cmd>call codeium#Clear()<CR>", desc = "Clear suggestion" },
+  --     -- WARN: <C-y> does'n work :(
+  --     -- { mode = { "i" }, "<C-y>", "<Cmd>call codeium#Accept()<CR>", desc = "Accept suggestion" },
+  --     { mode = { "i" }, "<C-n>", "<Cmd>call codeium#CycleCompletions(1)<CR>", desc = "Next suggestion" },
+  --     { mode = { "i" }, "<C-p>", "<Cmd>call codeium#CycleCompletions(-1)<CR>", desc = "Previous suggestion" },
+  --   },
+  -- },
 
   {
     "frankroeder/parrot.nvim",
