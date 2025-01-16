34c192bcc25cb584a68976a6d5863f8e9dc99f27
elio <elio@localhost.localdomain>
2025-01-16 17:03:16 +0000

    added image support for neovim

--- init.lua
+++ init.lua
@@ -117,6 +117,8 @@ require("lazy").setup({
   require("plugins.dropbar"),
 
   require("plugins.treewalker"),
+
+  require("plugins.image"),
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
