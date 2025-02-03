3bfce10a72cd76876bbea3a15e6c122665e30f45
elio <elio@localhost.localdomain>
2025-02-03 17:00:25 +0000

    fixed tab configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,9 +7,10 @@ vim.opt.foldmethod = "indent"
 vim.opt.foldenable = false
 vim.opt.foldlevel = 99
 vim.opt.shiftround = true -- Round indent
-vim.opt.shiftwidth = 2 -- Size of an indent
-vim.opt.tabstop = 2 -- Size of an indent
-vim.opt.expandtab = true -- tab to spaces
+vim.opt.expandtab = true -- expand tab input with spaces characters
+vim.opt.smartindent = true --  syntax aware indentations for newline inserts
+vim.opt.tabstop = 2 -- num of space characters per tab
+vim.opt.shiftwidth = 2 -- spaces per indentation level
 vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
 vim.opt.smartcase = true -- Don't ignore case with capitals
 vim.opt.smartindent = true -- Insert indents automatically
