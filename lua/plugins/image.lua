36cb84ee0e790dfccf3ffdc8992c568366363a29
elio <elio@localhost.localdomain>
2025-02-18 20:58:41 +0000

    replaced image viewing plugin

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,8 +1,13 @@
 ---@type LazyPluginSpec[]
 return {
+  -- {
+  --   "3rd/image.nvim",
+  --   build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+  --   opts = {},
+  -- },
   {
-    "3rd/image.nvim",
-    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    "Toprun123/PicVim",
+    lazy = false,
     opts = {},
   },
 }
