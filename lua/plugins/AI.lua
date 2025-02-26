d02015b3b805522c5ee8d2a6f00ec75bb0cd29aa
elio <elio@localhost.localdomain>
2025-02-26 21:28:22 +0000

    fixed ai plugin

--- lua/plugins/AI.lua
+++ lua/plugins/AI.lua
@@ -17,8 +17,26 @@ return {
     opts = {
       providers = {
         googleai = {
-          endpoint = "https://generativelanguage.googleapis.com/v1beta/models/{{model}}:streamGenerateContent?key={{secret}}",
+          -- endpoint = "https://generativelanguage.googleapis.com/v1beta/models/{{model}}:streamGenerateContent?key={{secret}}",
           secret = os.getenv("AI_API_KEY"),
+        },
+      },
+      agents = {
+        {
+          provider = "googleai",
+          name = "ChatGemini",
+          chat = true,
+          command = true,
+          model = { model = "gemini-2.0-flash", temperature = 0.8, top_p = 1 },
+          system_prompt = "You are a general AI assistant.\n\n"
+            .. "The user provided the additional info about how they would like you to respond:\n\n"
+            .. "- If you're unsure don't guess and say you don't know instead.\n"
+            .. "- Ask question if you need clarification to provide better answer.\n"
+            .. "- Think deeply and carefully from first principles step by step.\n"
+            .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+            .. "- Use Socratic method to improve your thinking and coding skills.\n"
+            .. "- Don't elide any code from your output if the answer requires coding.\n"
+            .. "- Take a deep breath; You've got this!\n",
         },
       },
     },
