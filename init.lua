281a06563cc882bb82b996159f21354e941153d9
elio <elio@localhost.localdomain>
2025-03-06 00:37:48 +0000

    added a way to add filetypes

--- init.lua
+++ init.lua
@@ -38,6 +38,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+vim.filetype.add({
+  pattern = {
+    [".*%.http"] = "http",
+  },
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
