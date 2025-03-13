fd4687f797b6197ef58db208f0cf7b19f5aa8153
elio <elio@localhost.localdomain>
2025-03-13 01:30:15 +0000

    commented one line

--- lua/plugins/AI.lua
+++ lua/plugins/AI.lua
@@ -7,7 +7,7 @@ return {
       keymaps = {
         accept_suggestion = "<Tab>",
         clear_suggestion = "<C-]>",
-        accept_word = "<C-j>",
+        -- accept_word = "<C-j>",
       },
     },
   },
