9df109b88832091493add452ceb027f5047cf2fc
elio <elio@localhost.localdomain>
2025-03-23 18:31:18 +0000

    added a scratch buffer

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -412,4 +412,21 @@ return {
     },
   },
   { "saecki/live-rename.nvim" },
+  -- use lazy.nvim
+  {
+    "LintaoAmons/scratch.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      { "ibhagwan/fzf-lua" }, --optional: if you want to use fzf-lua to pick scratch file. Recommanded, since it will order the files by modification datetime desc. (require rg)
+      { "nvim-telescope/telescope.nvim" }, -- optional: if you want to pick scratch file by telescope
+      { "stevearc/dressing.nvim" }, -- optional: to have the same UI shown in the GIF
+    },
+    opts = {
+      scratch_file_dir = vim.fn.stdpath("config") .. "/scratch.nvim",
+    },
+    keys = {
+      { "<leader>xs", "<cmd>Scratch<cr>", desc = "new Scratch" },
+      { "<leader>xS", "<cmd>ScratchOpen<cr>", desc = "Scratch open" },
+    },
+  },
 }
