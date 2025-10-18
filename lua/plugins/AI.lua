4a5a1f32c3a80d58d23227da476c1526a4a79677
Elio <90.elio@gmail.com>
2025-10-18 17:15:13 +0100

    refactor

--- lua/plugins/AI.lua
+++ lua/plugins/AI.lua
@@ -11,104 +11,16 @@ return {
       },
     },
   },
-  -- {
-  --   "folke/sidekick.nvim",
-  --   opts = {
-  --     -- add any options here
-  --     -- cli = {
-  --     --   mux = {
-  --     --     backend = "zellij",
-  --     --     enabled = true,
-  --     --   },
-  --     -- },
-  --     -- cli = {
-  --     --   prompts = {
-  --     --     refactor = "Please refactor {this} to be more maintainable",
-  --     --     security = "Review {file} for security vulnerabilities",
-  --     --     commit = "create a commit message following github standard and add an emoji, use the staged changes",
-  --     --   },
-  --     -- },
-  --   },
-  --   keys = {
-  --     {
-  --       "<tab>",
-  --       function()
-  --         -- if there is a next edit, jump to it, otherwise apply it if any
-  --         if not require("sidekick").nes_jump_or_apply() then
-  --           return "<Tab>" -- fallback to normal tab
-  --         end
-  --       end,
-  --       expr = true,
-  --       desc = "Goto/Apply Next Edit Suggestion",
-  --     },
-  --     {
-  --       "<leader>at",
-  --       function()
-  --         require("sidekick.cli").toggle()
-  --       end,
-  --       desc = "Sidekick Toggle CLI",
-  --     },
-  --     {
-  --       "<leader>as",
-  --       function()
-  --         require("sidekick.cli").select()
-  --       end,
-  --       -- Or to select only installed tools:
-  --       -- require("sidekick.cli").select({ filter = { installed = true } })
-  --       desc = "Select CLI",
-  --     },
-  --     {
-  --       "<leader>at",
-  --       function()
-  --         require("sidekick.cli").toggle({ name = "claude", focus = true })
-  --       end,
-  --       -- mode = { "x", "n" },
-  --       desc = "Sidekick Toggle Claude",
-  --     },
-  --     {
-  --       "<leader>av",
-  --       function()
-  --         require("sidekick.cli").send({ msg = "{selection}" })
-  --       end,
-  --       mode = { "x" },
-  --       desc = "Send Visual Selection",
-  --     },
-  --     {
-  --       "<leader>ap",
-  --       function()
-  --         require("sidekick.cli").prompt()
-  --       end,
-  --       mode = { "n", "x" },
-  --       desc = "Sidekick Select Prompt",
-  --     },
-  --     {
-  --       "<c-.>",
-  --       function()
-  --         require("sidekick.cli").focus()
-  --       end,
-  --       mode = { "n", "x", "i", "t" },
-  --       desc = "Sidekick Switch Focus",
-  --     },
-  --     -- Example of a keybinding to open Claude directly
-  --     -- {
-  --     --   "<leader>ac",
-  --     --   function()
-  --     --     require("sidekick.cli").toggle({ name = "claude", focus = true })
-  --     --   end,
-  --     --   desc = "Sidekick Toggle Claude",
-  --     -- },
-  --     {
-  --       "<leader>ac",
-  --       function()
-  --         require("sidekick.cli").send({
-  --           msg = "there are new changes, create a commit message following github standard and add an emoji, use the staged changes",
-  --         })
-  --       end,
-  --       desc = "Sidekick commit",
-  --     },
-  --   },
-  -- },
-
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    config = function()
+      require("mcphub").setup()
+    end,
+  },
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
@@ -146,6 +58,16 @@ return {
           max_tokens = 8192,
         },
       },
+      system_prompt = function()
+        local hub = require("mcphub").get_hub_instance()
+        return hub and hub:get_active_servers_prompt() or ""
+      end,
+      -- Using function prevents requiring mcphub before it's loaded
+      custom_tools = function()
+        return {
+          require("mcphub.extensions.avante").mcp_tool(),
+        }
+      end,
       behaviour = {
         auto_suggestions = false, -- Experimental stage
         auto_set_highlight_group = true,
@@ -156,6 +78,7 @@ return {
     },
     build = "make", -- if you want to build from source then do make BUILD_FROM_SOURCE=true
     dependencies = {
+      "ravitemer/mcphub.nvim",
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
