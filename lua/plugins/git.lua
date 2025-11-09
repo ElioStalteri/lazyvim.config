e695f6725836886a2653c7ff1ac4809590b39c5d
Elio <90.elio@gmail.com>
2025-11-09 16:04:09 +0000

    removed conflic res

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -20,21 +20,21 @@ return {
   --     { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
   --   },
   -- },
-  {
-    "StackInTheWild/headhunter.nvim",
-    lazy = true,
-    opts = {
-      register_keymaps = false, -- Disable internal keymaps if using lazy.nvim keys
-    },
-    keys = {
-      { "<leader>gq", "<cmd>HeadhunterQuickFix<cr>", desc = "Conflicts Quickfix list" },
-      { "<leader>gn", "<cmd>HeadhunterNext<cr>", desc = "Go to next Conflict" },
-      { "<leader>gp", "<cmd>HeadhunterPrevious<cr>", desc = "Go to previous Conflict" },
-      { "<leader>gh", "<cmd>HeadhunterTakeHead<cr>", desc = "Take changes from HEAD" },
-      { "<leader>go", "<cmd>HeadhunterTakeOrigin<cr>", desc = "Take changes from origin" },
-      { "<leader>gb", "<cmd>HeadhunterTakeBoth<cr>", desc = "Take both changes" },
-    },
-  },
+  -- {
+  --   "StackInTheWild/headhunter.nvim",
+  --   lazy = true,
+  --   opts = {
+  --     register_keymaps = false, -- Disable internal keymaps if using lazy.nvim keys
+  --   },
+  --   keys = {
+  --     { "<leader>gq", "<cmd>HeadhunterQuickFix<cr>", desc = "Conflicts Quickfix list" },
+  --     { "<leader>gn", "<cmd>HeadhunterNext<cr>", desc = "Go to next Conflict" },
+  --     { "<leader>gp", "<cmd>HeadhunterPrevious<cr>", desc = "Go to previous Conflict" },
+  --     { "<leader>gh", "<cmd>HeadhunterTakeHead<cr>", desc = "Take changes from HEAD" },
+  --     { "<leader>go", "<cmd>HeadhunterTakeOrigin<cr>", desc = "Take changes from origin" },
+  --     { "<leader>gb", "<cmd>HeadhunterTakeBoth<cr>", desc = "Take both changes" },
+  --   },
+  -- },
   {
     "FabijanZulj/blame.nvim",
     lazy = false,
